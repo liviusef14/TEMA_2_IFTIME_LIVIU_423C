f4ffb19dfe0333fa0d797e839afa66dc4539a05c
liviusef14 <57236777+liviusef14@users.noreply.github.com>
2020-01-05 14:26:15 +0200

    Add files via upload

--- Semnale_si_Programare_T2_Iftime_Liviu_423C.m
+++ Semnale_si_Programare_T2_Iftime_Liviu_423C.m
@@ -4,7 +4,7 @@ syms t n % definim astfel pentru ca avem nevoie pentru functia int care calculea
 P=40;  % Perioada semnalului
 w=2*pi/P; 
 D=11;  % Durata semnalului dreptunghic
-rezolutie=1/30; 
+rezolutie=1/20; 
 % Rezolutie aleasa ca sa avem imagine mai exacta, dar si sa nu avem timp de rulare foarte mare
 semnal=[ones(1,D/rezolutie),zeros(1,(P-D)/rezolutie),ones(1,D/rezolutie),zeros(1,(P-D)/rezolutie),ones(1,D/rezolutie),zeros(1,(P-D)/rezolutie)];
 % Semnalul dreptunghiular este format doar din valori de 1 si 0 asa ca am
@@ -13,28 +13,25 @@ semnal=[ones(1,D/rezolutie),zeros(1,(P-D)/rezolutie),ones(1,D/rezolutie),zeros(1
 timp=0:rezolutie:120-rezolutie; 
 % Durata de timp pentru care vom face calculele
 
-n=1:50; % numarul de coeficienti
+n=-50:-1; % numarul de coeficienti
 
-a0 =(1/P)*int(1,t,0,D)  % formula = integrala din 1 dt de la 0 la D
-% de la D la P nu avem nevoie pentru ca atunci valoarea este 0 deci
-% integrarea ar fi degeaba
-an =(2/P)*int(1*cos(w*n*t),t,0,D) % formula
-bn =(2/P)*int(1*sin(w*n*t),t,0,D) % formula
-a0=double(a0);
-an=double(an);
-bn=double(bn);
-for a=1:50
-    cn1(a)=an(abs(a-51))+i*bn(abs(a-51));
-    cn2(a)=an(a)+i*bn(a);
-end
+cnminus=int(power( exp(1),-1j*n*w*t),t,0,D);
+m=0; %coeficientul exponential cu minus (nu-l afisam), ci doar incadram cn intre el si
+%coeficientul cu plus;
+c0=int(power( exp(1),-1j*m*w*t),t,0,D);
+p=1:50;
+cnplus= int(power( exp(1),-1j*p*w*t),t,0,D) %coeficientul exponential afisat;
 
-cn=[cn1,a0,cn2]; % Valorile coeficientilor
+cn=[cnminus,c0,cnplus];
+cnminus=double(cnminus);
+cnplus=double(cnplus);
+c0=double(c0);
+cn=double(cn);
 
-semnal2=a0;
+semnal2=c0/P;
 for i=1:50
-    semnal2=semnal2+an(i)*cos(w*i*timp)+bn(i)*sin(w*i*timp);
+semnal2=semnal2+2*cnplus(i)*power(exp(1),1j*i*w*timp)/P;
 end
-% am recreat semnalul insumand coeficientii calculati
 
 figure(1)
 plot(timp,semnal);
@@ -47,4 +44,4 @@ hold off
 f=-50:50;
 figure(2)
 stem(f,abs(cn));
-title('Spectrul semnalului x(t) care se obtine din calculul a 51 de coeficienti')
+title('Spectrul semnalului x(t) care obtine din calculul a 51 de coeficienti')
